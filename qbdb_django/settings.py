4e6e44caaf1aeaf1986c3ca4eae5d2ef7969aefa
Jerry Vinokurov <yury.vinokurov@cfpb.gov>
2015-05-06 12:06:28 -0400

    hide the secret key

--- qbdb_django/settings.py
+++ qbdb_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+=2=1^ih$xcgckl=89i07mvvp20y%hl$%(0=x^%hhbqsx694a='
+from local_settings import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
