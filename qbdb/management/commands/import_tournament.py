77068908ae2935d467481b7ae1a080238e994266
Jerry Vinokurov <yury.vinokurov@cfpb.gov>
2017-04-05 12:32:21 -0400

    changed import_tournament to allow for multiple file spec

--- qbdb/management/commands/import_tournament.py
+++ qbdb/management/commands/import_tournament.py
@@ -9,15 +9,15 @@ class Command(BaseCommand):
     help = 'Imports specified tournament JSON file into the database.'
 
     def add_arguments(self, parser):
-        parser.add_argument('tournament_json_file', nargs='?')
+        parser.add_argument('tournament_json_files', nargs='*')
 
     def handle(self, *args, **options):
 
-        if 'tournament_json_file' in options:
-            tour_file = options['tournament_json_file']
-            tour_data = json.load(open(tour_file, 'r'))
-            put_tournament_into_db(tour_data)
-            print 'imported', tour_file
+        if 'tournament_json_files' in options:
+            for tour_file in options['tournament_json_files']:
+                tour_data = json.load(open(tour_file, 'r'))
+                put_tournament_into_db(tour_data)
+                print 'imported', tour_file
 
         else:
-            raise CommandError('Must supply file to import.')
+            raise CommandError('Must supply one or more files to import.')
